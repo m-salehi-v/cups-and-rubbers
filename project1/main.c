92d42d2cc5f632ac672c4de00e4218a7eef274be
mammadino <mohammadsalehivaziri@gmail.com>
2019-12-11 21:37:15 +0330

    rand_movement func was added
    and some other changesto the previous version

--- project1/main.c
+++ project1/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+
+
+int matrix_x, matrix_y, police_station, policemen[10], robber_x, robber_y;
+int matrix[20][20];
+void rand_movement(int *x, int *y);
+void display_matrix();
 
 int main() {
 
-    int matrix_x, matrix_y, police_station, policemen[10], robber_x, robber_y;
+
     printf("enter the number of rows and columns:\n");
     scanf("%d %d", &matrix_x, &matrix_y);
     printf("enter the number of police stations and policemen for each one:\n");
@@ -12,7 +19,8 @@ int main() {
     for (int i = 1; i <= police_station; i++) {
         scanf("%d", &policemen[i]);
     }
-    int matrix[matrix_x][matrix_y];
+
+    //matrix initialization to 0
     for (int i = 0; i < matrix_x; i++)
         for (int j = 0; j < matrix_y; ++j)
             matrix[i][j] = 0;
@@ -41,8 +49,46 @@ int main() {
         }
 
     }
+    display_matrix();
+    /*
+    just checking the rand_movement func
+    while (1) {
+        for (int i = 0; i < matrix_x; ++i) {
+            for (int j = 0; j < matrix_y; ++j) {
+                if (matrix[i][j] != 0) {
+                    rand_movement(&i, &j);
+                    display_matrix();
+                    printf("\n\n");
+                    sleep(1);
+                    system("cls");
 
-    //displaying the matrix
+                }
+
+            }
+        }
+    }
+    */
+    return 0;
+}
+
+void rand_movement(int *x, int *y) {
+    int tmp = matrix[*x][*y], k;
+    matrix[*x][*y] = 0;
+    while (1){
+        k = (rand() % 3) - 1;
+        *x += k;
+        *y += k;
+        if((*x >= 0) && (*x < matrix_x) && (*y >= 0) && (*y < matrix_y) && (matrix[*x][*y] == 0)){
+            matrix[*x][*y] = tmp;
+            break;
+        }
+        *x -= k;
+        *y -= k;
+    }
+
+
+}
+void display_matrix() {
     for (int i = 0; i < matrix_x; i++) {
         for (int j = 0; j < matrix_y; ++j) {
             if (matrix[i][j] == 0)
@@ -54,6 +100,4 @@ int main() {
         }
         printf("\n");
     }
-
-    return 0;
 }
